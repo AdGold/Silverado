dc7c14dea5f036e62cb4f3e6bfefd05b8df811d0
Adrian Goldwaser <agold@hotmail.com>
2014-10-02 15:09:43 +1000

    fixed movies page

--- movies.js
+++ movies.js
@@ -1,17 +1,20 @@
 $(document).ready(function(){
-    alert(Server);
-    var types = ['RC','AC','FO','CH'];
-    for (t in types)
+    function getMovie(type)
     {
-        $.post(
+       $.post(
         "http://"+server+"/~e54061/wp/movie-service.php" ,
         {
             CRC:'s3493577',
-            filmID:types[t]
+            filmID:type
         },
         function(data){
-            $('#'+types[t]).html(data);
+            $('#'+type).html(data);
         });
+    }
+    var types = ['RC','AC','FO','CH'];
+    for (t in types)
+    {
+        getMovie(types[t]);
     }
 });
 
