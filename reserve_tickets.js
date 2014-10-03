19edac963d71aced5d273dea16f30727b88600d5
Adrian Goldwaser <agold@hotmail.com>
2014-10-03 15:38:28 +1000

    fixed post

--- reserve_tickets.js
+++ reserve_tickets.js
@@ -1,6 +1,6 @@
 reserve = angular.module('reserve', []);
 
-reserve.controller('reserveController', function($scope) {
+reserve.controller('reserveController', function($scope, $http) {
     $scope.validate = function() {
         var check = $("#code").val();
         if (/\d{5}-\d{5}-[A-Z]{2}/g.test(check))
@@ -8,11 +8,13 @@ reserve.controller('reserveController', function($scope) {
             $scope.result = "Checking code...";
 
             $http.post("checkcode.php", { code: check }).success(function(data) {
+                alert('wooo');
                 if (data == "1")
                     $scope.result = "Success :)";
                 else
                     $scope.result = "Invalid code";
             });
+            alert('sent post :) ');
         }
         else if (check.length == 0)
             $scope.result = "";
