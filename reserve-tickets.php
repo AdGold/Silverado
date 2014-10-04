5cc755d84b8c60e54681c951c15dc28efda4373c
Adrian Goldwaser <agold@hotmail.com>
2014-10-04 14:52:46 +1000

    fixed writing to file

--- reserve-tickets.php
+++ reserve-tickets.php
@@ -1,26 +1,33 @@
 <?php
 session_start();
+//*
+$email = $_GET['email'];
+$phone = $_GET['phone'];
+$name = $_GET['name'];
+$code = $_GET['code'];
+/*/
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 $name = $_POST['name'];
 $code = $_POST['code'];
+*/
 $page_title = "Tickets Reserved";
 $hide_cart = true;
 include_once("header.php");
 
+$_SESSION['tickets'] = array('Monday'=>array('Maxima'=>array('9'=>array(array('A01', 'Beanbag'), array('E07', 'FirstClass-Adult')))),'Saturday'=>array('Rivola'=>array('4'=>array(array('D05','Conc')))));
+                            
 $xml = simplexml_load_file("seats.xml");
 foreach($_SESSION['tickets'] as $day => $daytickets)
 {
-    $xday = $xml->xpath($day);
     foreach($daytickets as $cinema => $cinematickets)
     {
-        $xcinema = $xday->xpath($cinema);
         foreach ($cinematickets as $time => $timetickets)
         {
-            $xtime = $xcinema->xpath('x' . $time);
             foreach ($timetickets as $ticket)
             {
-                $xseat = $xtime->xpath($ticket[0]);
+                $result = $xml->xpath("$day/$cinema/x$time/$ticket[0]");
+                $xseat = $result[0];
                 $xseat['full'] = 'true';
                 $xseat->addChild('name', $name);
                 $xseat->addChild('phone', $phone);
