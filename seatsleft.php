6f19fcfb940ec3481a983215e2540181ff19a956
Adrian Goldwaser <agold@hotmail.com>
2014-10-04 14:20:30 +1000

    neatened seatsleft.php

--- seatsleft.php
+++ seatsleft.php
@@ -3,40 +3,16 @@ session_start();
 $cinema = $_POST['cinema'];
 $day = $_POST['day'];
 $time = $_POST['time'];
-//$cinema = 'Rivola';
+//$cinema = 'Maxima';
 //$day = 'Monday';
 //$time = '9';
 $xml = simplexml_load_file("seats.xml");
-$seats = array();
 
-foreach($xml->children() as $xmlday)
+$result = $xml->xpath("/seats/$day/$cinema/x$time");
+$xmlseats = $result[0];
+foreach($xmlseats->children() as $xmlseat)
 {
-    if ($xmlday->getName() == $day)
-    {
-        foreach($xmlday->children() as $xmlcinema)
-        {
-            if ($xmlcinema->getName() == $cinema)
-            {
-                foreach($xmlcinema->children() as $xmltime)
-                {
-                    if ($xmltime->getName() == "x" . $time)
-                    {
-                        foreach($xmltime->children() as $xmlseat)
-                        {
-                            if ($xmlseat['full'] == 'false')
-                                array_push($seats, $xmlseat->getName());
-                        }
-                        break;
-                    }
-                }
-                break;
-            }
-        }
-        break;
-    }
-}
-foreach($seats as $seat)
-{
-    echo $seat . " ";
+    if ($xmlseat['full'] == 'false')
+        echo $xmlseat->getName() . ' ';
 }
 ?>
